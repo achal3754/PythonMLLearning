11900d9690abe9d21cdddb15c5ddbb756442f9f1
Achal Kumar <kumarachal3754@yahoo.com>
2020-09-03 11:22:57 +0530

    Create MyTestModel.py

---
+++ MyTestModel.py
@@ -0,0 +1,15 @@
+import time
+import logging
+import sys
+import os
+from datetime import datetime
+logging.basicConfig(filename="mylog.log", level=logging.INFO)
+
+
+    
+def Main():
+
+      
+
+####################### MAIN ############################
+Main()
